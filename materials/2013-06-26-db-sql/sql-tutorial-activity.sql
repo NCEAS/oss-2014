d93c15d2339d4e2a32d5cf85d869fe3b12cd1a00
Matt Jones <gitcode@magisa.org>
2013-06-26 00:34:54 -0800

    Reformatted comments.

--- materials/2013-06-26-db-sql/sql-tutorial-activity.sql
+++ materials/2013-06-26-db-sql/sql-tutorial-activity.sql
@@ -1,18 +1,17 @@
 SQL tutorial notes
 ==================
 
-# Create postgres user accounts for all participants
-    # createuser -d -e -E -l -r -s <username>
-    for un in `cat ~jones/nceasSI-accounts`; do echo "Creating $un..."; createuser -d -e -E -l -r -s $un; echo "Done with $un."; done
+-- Create postgres user accounts for all participants
+    -- createuser -d -e -E -l -r -s <username>
+    -- for un in `cat ~jones/nceasSI-accounts`; do echo "Creating $un..."; createuser -d -e -E -l -r -s $un; echo "Done with $un."; done
 
-# Create a database for yourself and connect to it
+-- Create a database for yourself and connect to it
 	createdb testdb_jones
 	psql testdb_jones
 	\?
 	\d
 
-# Create a table and insert data
-# CREATE TABLE
+-- Create a table
     CREATE TABLE sites (
 		siteid INT8,
 		altitude INT8, 
@@ -20,12 +19,12 @@ SQL tutorial notes
 		CONSTRAINT site_pk PRIMARY KEY (siteid)
 	);
 
-# INSPECT THE TABLE THAT WAS CREATED
+-- INSPECT THE TABLE THAT WAS CREATED
 	\d
 	\d sites
 	SELECT * FROM sites;
 
-# INSERT
+-- INSERT SOME DATA INTO THE TABLE
     INSERT INTO sites (siteid, altitude, habitat) VALUES (1, 722, 'scrub');
     INSERT INTO sites (siteid, altitude, habitat) VALUES (2, 805, 'scrub');
     INSERT INTO sites (siteid, altitude, habitat) VALUES (3, 887, 'grass');
@@ -35,48 +34,48 @@ SQL tutorial notes
     INSERT INTO sites (siteid, altitude, habitat) VALUES (7, 121, 'forest');
     INSERT INTO sites (siteid, altitude, habitat) VALUES (8, 108, 'grass');
 
-# SIMPLE SELECTs
+-- SIMPLE SELECTs
 	SELECT * from sites;
 	SELECT habitat from sites;
 	SELECT habitat from sites order by habitat;
 
-# SELECT with a constraint
+-- SELECT with a constraint
 	SELECT * from sites where altitude > 500;
 	SELECT * from sites where habitat = 'scrub';
 
-# SELECT with a more complex constraint
+-- SELECT with a more complex constraint
 	SELECT * from sites where habitat = 'scrub' AND altitude > 500;
 
-# SELECT with an aggregation function using group by
+-- SELECT with an aggregation function using group by
 	SELECT habitat, count(*) from sites group by habitat order by habitat;
 
-# INTEGRITY CONSTRAINTS: PRIMARY KEY ENFORCEMENT
+-- INTEGRITY CONSTRAINTS: PRIMARY KEY ENFORCEMENT
 	INSERT INTO sites (siteid, altitude, habitat) VALUES (1, 721, 'scrub');
 
-# UPDATE
+-- UPDATE
 	UPDATE sites set altitude=721 where siteid=1;
 
-# DELETE
+-- DELETE
 	INSERT INTO sites (siteid, altitude, habitat) VALUES (9, 121, 'forest');
 	SELECT count(*) from sites;
 	DELETE from sites where siteid=9;
 
-# TRANSACTIONS
+-- TRANSACTIONS
 	INSERT INTO sites (siteid, altitude, habitat) VALUES (9, 121, 'forest');
 	START TRANSACTION;
     DELETE FROM sites WHERE siteid > 7;
 	SELECT * from sites order by siteid;
-	# Oh Crap!
+	-- Oh Crap!
 	ROLLBACK;
 	SELECT * from sites order by siteid;
     
-	# Try again to delete the right set of rows
+	-- Try again to delete the right set of rows
 	START TRANSACTION;
     DELETE FROM sites WHERE siteid > 8;
 	SELECT * from sites order by siteid;
 	COMMIT;
 
-# CREATE ANOTHER TABLE THAT WILL BE RELATED TO site
+-- CREATE ANOTHER TABLE THAT WILL BE RELATED TO site
     CREATE TABLE plotobs (
 		obsid INT8,
 		siteid INT8, 
@@ -90,10 +89,10 @@ SQL tutorial notes
 		CONSTRAINT plotobs_site_fk FOREIGN KEY (siteid) REFERENCES sites
 	);
 
-# Batch load data from a CSV file
+-- Batch load data from a CSV file
 
 	
 
-# ACID
-# CRUD
+-- ACID
+-- CRUD
 
