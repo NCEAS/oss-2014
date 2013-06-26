af22abe02b04851cb8c39aea80354dc6720e9122
Matt Jones <gitcode@magisa.org>
2013-06-25 22:37:30 -0800

    Format gbif messages in a cleaner manner, show progress, eliminate curl progress meter, factor out query string to variable at top of script.

--- materials/gbif-sh/gbif.sh
+++ materials/gbif-sh/gbif.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+#
+# The query to be run
+SEARCH="Puma+concolor"
+
 OUT="dwcdata.csv"
 CURRENT="dwcdata.xml"
 BASE=http://data.gbif.org/ws/rest/occurrence
-URLC="${BASE}/count?scientificname=Puma+concolor"
-URLL=http://data.gbif.org/ws/rest/occurrence/list?scientificname=Puma+concolor\&format=darwin
+URLC="${BASE}/count?scientificname=${SEARCH}"
+URLO=http://data.gbif.org/ws/rest/occurrence/list?scientificname=${SEARCH}\&format=darwin
+URLL=${URLO}
 
 echo "to:InsititutionCode,to:CollectionCode,to:catalogNumber,tn:nameComplete" > $OUT
 
 # Query GBIF to find out how many records match the search
-COUNT=`curl $URLC | xmlstarlet sel -t -m "//gbif:summary" -v "@totalMatched"`
-echo $COUNT
-
+COUNT=`curl -s $URLC | xmlstarlet sel -t -m "//gbif:summary" -v "@totalMatched"`
+echo "Total records: $COUNT"
 
 # Loop over blocks of those records, extracting the fields of interest into a CSV
 COUNTER=0
 while [  $COUNTER -lt $COUNT ]; do
+    #echo $URLL
+    echo "Requesting records starting at $COUNTER..."
+	curl -s $URLL | xmlstarlet sel -t -m "//to:TaxonOccurrence" -v "concat(to:institutionCode, ',', to:collectionCode, ',', to:catalogNumber, ',', //tn:nameComplete, '~')" | tr '~' "\n" >> $OUT
     let COUNTER=COUNTER+1000 
-    echo The counter is $COUNTER
-    echo $URLL
-	curl $URLL > $CURRENT
-    cat $CURRENT | xmlstarlet sel -t -m "//to:TaxonOccurrence" -v "concat(to:institutionCode, ',', to:collectionCode, ',', to:catalogNumber, ',', //tn:nameComplete, '~')" | tr '~' "\n" >> $OUT
-	export URLL=$URLL\&startindex=${COUNTER}
-    #URLL=`cat $CURRENT | xmlstarlet sel -t -v "//gbif:nextRequestUrl"`
-    echo "New Endpoint is: $URLL"
+	export URLL=${URLO}\&startindex=${COUNTER}
 done
